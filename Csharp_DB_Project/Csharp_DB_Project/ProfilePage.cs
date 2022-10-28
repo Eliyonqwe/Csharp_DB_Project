234d454c2ff5273c6a11de74439493ee86c900af
YeabsiraEz <yeabisraesayas@gmail.com>
2022-10-28 09:37:38 +0300

    error provider added to profile page

--- Csharp_DB_Project/Csharp_DB_Project/ProfilePage.cs
+++ Csharp_DB_Project/Csharp_DB_Project/ProfilePage.cs
@@ -1,5 +1,7 @@
 ﻿using Csharp_DB_Project.Classes;
 using System;
+using System.Collections;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace Csharp_DB_Project
@@ -61,22 +63,86 @@ namespace Csharp_DB_Project
         }
         private void btn_update_Click(object sender, EventArgs e)
         {
-            if (changeIndicator == 0)
+            Regex fname_error = new Regex(@"[a-z A-Z]{" + txt_fname.Text.Length + @"}");
+            Regex number_error = new Regex(@"[0-9]{" + txt_phone.Text.Length + @"}");
+            Regex lname_error = new Regex(@"[a-z A-Z]{" + txt_lname.Text.Length + @"}");
+
+
+
+            if ((txt_fname.Text.Length != 0 && txt_lname.Text.Length != 0 && txt_password.Text.Length != 0 && txt_phone.Text.Length != 0 && txt_uname.Text.Length != 0) && (number_error.IsMatch(txt_phone.Text) && fname_error.IsMatch(txt_fname.Text) && lname_error.IsMatch(txt_lname.Text)))
+
             {
-                MessageBox.Show("Nothing has been altered/changed!");
+
+                User u = new User();
+                u.firstName = txt_fname.Text;
+                u.lastName = txt_lname.Text;
+                u.username = txt_uname.Text;
+                u.phoneNumber = txt_phone.Text;
+                u.password = txt_password.Text;
+                
+
+                String conStatus = u.Save();
+
+                if (conStatus == "0")
+                    MessageBox.Show("Account crreated Succesfully!");
+                else
+                    MessageBox.Show(conStatus);
+
             }
+
             else
             {
 
-                String status = u.updateUser(Convert.ToInt32(txt_id.Text), txt_fname.Text, txt_lname.Text, txt_uname.Text, txt_password.Text, txt_phone.Text);
-
-                if (status == "0")
+                if (string.IsNullOrEmpty(txt_fname.Text))
                 {
-                    MessageBox.Show("Change has been recorded!");
-                    changeIndicator = 0;
+                    errorProvider1.SetError(txt_fname, "please provide a Name ");
+
                 }
-                else
-                    MessageBox.Show(status);
+
+                else if (fname_error.IsMatch(txt_fname.Text) == false)
+                {
+                    errorProvider1.SetError(txt_fname, "please enter a letters only ");
+                }
+
+                if (string.IsNullOrEmpty(txt_lname.Text))
+                {
+                    errorProvider1.SetError(txt_lname, "please provide a Name ");
+
+
+                }
+                else if (lname_error.IsMatch(txt_lname.Text) == false)
+                {
+                    errorProvider1.SetError(txt_lname, "please enter letters only ");
+                }
+
+                if (string.IsNullOrEmpty(txt_phone.Text))
+                {
+                    errorProvider1.SetError(txt_phone, "please provide a phone number ");
+
+
+                }
+                else if (number_error.IsMatch(txt_phone.Text) == false)
+                {
+                    errorProvider1.SetError(txt_phone, "please enter a number only ");
+                }
+
+                if (string.IsNullOrEmpty(txt_password.Text))
+                {
+                    errorProvider1.SetError(txt_password, "please provide a password ");
+
+
+                }
+                if (string.IsNullOrEmpty(txt_uname.Text))
+                {
+                    errorProvider1.SetError(txt_uname, "please provide a username ");
+
+
+                }
+                else if (txt_uname.Text == u.username)
+                {
+                    errorProvider1.SetError(txt_uname, "user name already exist");
+                }
+                
 
             }
         }
