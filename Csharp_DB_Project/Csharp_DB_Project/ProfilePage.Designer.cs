9924cadb3e62ccbfecc07f52e36ab359b3bd5117
leuldawit <97153381+leuldawit@users.noreply.github.com>
2022-10-28 12:23:12 +0300

    Revert "Merge pull request #19 from YeabsiraEz/main"
    This reverts commit 7b30e735fc337b8e991548559ecfb035dd908484, reversing
    changes made to b5b6f48d88ab2417055d4373431a24af50a61853.

--- Csharp_DB_Project/Csharp_DB_Project/ProfilePage.Designer.cs
+++ Csharp_DB_Project/Csharp_DB_Project/ProfilePage.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.txt_phone = new System.Windows.Forms.TextBox();
             this.txt_uname = new System.Windows.Forms.TextBox();
             this.txt_password = new System.Windows.Forms.TextBox();
@@ -45,9 +44,7 @@
             this.btn_homepage = new System.Windows.Forms.Button();
             this.txt_lname = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
             // txt_phone
@@ -225,10 +222,6 @@
             this.label1.TabIndex = 73;
             this.label1.Text = "Last Name";
             // 
-            // errorProvider1
-            // 
-            this.errorProvider1.ContainerControl = this;
-            // 
             // ProfilePage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -256,7 +249,6 @@
             this.Text = "ProfilePage";
             this.Load += new System.EventHandler(this.ProfilePage_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -280,6 +272,5 @@
         private System.Windows.Forms.Button btn_homepage;
         private System.Windows.Forms.TextBox txt_lname;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ErrorProvider errorProvider1;
     }
 }
