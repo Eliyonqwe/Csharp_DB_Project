9f94f15e79cbe5013e80ef6cdb173d7fc5c58423
eliyon <55352187+Eliyonqwe@users.noreply.github.com>
2022-10-28 22:10:55 +0300

    final
    Co-Authored-By: leuldawit <97153381+leuldawit@users.noreply.github.com>
    Co-Authored-By: YeabsiraEz <107886988+YeabsiraEz@users.noreply.github.com>
    Co-Authored-By: Michyas20 <101133166+Michyas20@users.noreply.github.com>

--- Csharp_DB_Project/Project.sql
+++ Csharp_DB_Project/Project.sql
@@ -8,20 +8,19 @@ create table admin(
 	password varchar(30) collate SQL_Latin1_general_cp1_cs_as NOT NULL -- Makes password column case sensetive
 );
 
-
 create table users(
 	userID int primary key identity(1,1),
 	firstName varchar(30) NOT NULL,
 	lastName varchar(30) NOT NULL,
-	--age int check(age>=18),
 	gender varchar(10),	
 	username varchar(30) NOT NULL UNIQUE,
 	password varchar(30) collate SQL_Latin1_general_cp1_cs_as NOT NULL, -- Makes password column case sensetive
 	phoneNumber varchar(15),
 	balance money default 0,
-	picName varchar(30),
-	profilepic image
+	picLocation varchar(30),
+	age int check(age>=18)
 );
+select *from users
 
 create table company(
 	companyID int primary key identity(1,1),
@@ -68,20 +67,20 @@ select *from stockListing
 select *from company
 select *from offer
 select *from orders
+
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 --Views
 
-alter view offerData as select  ou.FIrstName+' '+ ou.LastName as Offerer,su.FIrstName+' '+ su.LastName as Offeree, offerID ,offeringUserID, sellerUserID companyName, companyType, amount, price, offerAmount,offerStatus  from offer o join stockListing s on o.StockID = s.stockID join company c on c.companyID = s.CompanyID join users ou on ou.UserID = o.offeringUserID join users su on su.UserID = o.sellerUserID;
+alter view offerData as select  ou.FIrstName+' '+ ou.LastName as Offerer,su.FIrstName+' '+ su.LastName as Offeree, offerID ,offeringUserID, sellerUserID, companyName, companyType, amount, price, offerAmount,offerStatus  from offer o join stockListing s on o.StockID = s.stockID join company c on c.companyID = s.CompanyID join users ou on ou.UserID = o.offeringUserID join users su on su.UserID = o.sellerUserID;
 alter view offerList as select offerID ,offeringUserID, companyName, companyType, amount, price, offerAmount,offerStatus from offer o join stockListing s on o.StockID = s.stockID join company c on c.companyID = s.CompanyID;
-alter view orderData as select  ou.FIrstName+' '+ ou.LastName as Orderer,su.FIrstName+' '+ su.LastName as Orderee, orderID ,orderingUserID, sellerUserID companyName, companyType, amount, price, orderAmount  from orders o join stockListing s on o.StockID = s.stockID join company c on c.companyID = s.CompanyID join users ou on ou.UserID = o.orderingUserID join users su on su.UserID = o.sellerUserID;
+alter view orderData as select  ou.FIrstName+' '+ ou.LastName as Orderer,su.FIrstName+' '+ su.LastName as Orderee, orderID ,orderingUserID, sellerUserID, companyName, companyType, amount, price, orderAmount  from orders o join stockListing s on o.StockID = s.stockID join company c on c.companyID = s.CompanyID join users ou on ou.UserID = o.orderingUserID join users su on su.UserID = o.sellerUserID;
 alter view orderList as select orderinguserID, sellerUserID, companyName, companyType, amount as stockAmount,orderAmount as orderingPrice, orderID from orders o join stockListing s on o.StockID = s.stockID join company c on c.companyID = s.CompanyID;
 alter view viewAllListing as select  userID, stockID, companyName, companyType, amount, price, status from stockListing s join company c on s.companyID = c.companyID;
 alter view viewListing as select  username, stockID, c.companyID, companyName, companyType, amount, price from company c join stockListing s on c.companyID = s.companyID join users u on s.userID = u.userID;
 alter view viewListings as select  username, stockID, c.companyID, companyName, companyType, amount, price,status from company c join stockListing s on c.companyID = s.companyID join users u on s.userID = u.userID;
 alter view viewOffers as select companyName, companyType, amount, price as askingPrice, offeramount, offerstatus, offerID, offeringUserid, selleruserID, s.stockID from offer o join stockListing s on o.stockID = s.stockID join company c on c.companyID = s.companyID;
-
 
 
 --Procs
@@ -90,23 +89,23 @@ alter view viewOffers as select companyName, companyType, amount, price as askin
 -------------------------------------------------------------------------------------------------------------------------------------------
 
 alter procedure registerUser
-@fname varchar(30), @lname varchar(30), @gender varchar(10),@uname varchar(30),@passwd varchar(30), @phoneno varchar(30), @balance money, @imgLocation varchar(30), @img image
+@fname varchar(30), @lname varchar(30), @gender varchar(10),@uname varchar(30),@passwd varchar(30), @phoneno varchar(30), @balance money, @imgLocation varchar(30), @dateofBirth date
 as
 begin
-	--declare @age int = (select dbo.getAge(@date))
-	insert into users values(@fname,@lname, @gender,@uname,@passwd, @phoneno, @balance, @imgLocation , @img)
+	declare @age int  = (select dbo.getAge(@dateOfBirth))
+	insert into users values(@fname,@lname, @gender,@uname,@passwd, @phoneno, @balance, @imgLocation , @age)
 end
-
 
 create proc updateBalance
 @uname varchar(30), @balance money
 as
 	update users set balance = @balance where username = @uname
 
-create proc updateUser
-@id int, @fname varchar(30),@lname varchar(30),@uname varchar(30),@password varchar(30),@phone varchar(15)
+alter proc updateUser
+@id int, @fname varchar(30),@lname varchar(30), @gender varchar(10), @uname varchar(30),@password varchar(30),@phone varchar(15), @age int
 as
-	update users set firstName = @fname, lastName = @lname, username = @uname, password = @password, phoneNumber = @phone where userID = @id
+	update users set firstName = @fname, lastName = @lname, gender = @gender, username = @uname, password = @password, phoneNumber = @phone, age = @age where userID = @id
+
 
 create procedure viewAllUsers
 as
@@ -117,11 +116,12 @@ create proc viewUser
 @username varchar(30)
 as
 	select *from users where username = @username	
+	/*
 alter procedure searchUser
 @search varchar(30)
 as
 	select *from users where (concat(firstName, lastName, phoneNumber, username)) like '%'+@search+'%'
-
+	*/
 alter proc deleteUser
 @id int
 as
@@ -232,20 +232,14 @@ alter proc addOffer
 @offeringID int, @sellerID int, @stockID int,@offerAmount money
 as
 begin
-   declare @balance money 
-	select @balance = balance from users where userID = @offeringID
-
-   if @balance < @offerAmount
-		Raiserror('Error: Insufficient Balance!',11,1)
-   
-   else
 	 insert into offer(offeringUserID, sellerUserID, stockID,offerAmount) values(@offeringID, @sellerID, @stockID,@offerAmount)
 end
-
+/*
 create procedure searchOffersForAdmin
 @search varchar(30)
 as
 	select *from offerData where (concat(offerer,offeree, companyName, companyType, offerStatus)) like '%'+@search+'%' 
+*/
 create proc searchofferforuser
 @search varchar(30),@userID int
 as
@@ -266,13 +260,6 @@ alter proc updateOffer
 @offerID int, @userID int, @offerAmount money
 as
 begin
-	declare @balance money 
-	select @balance = balance from users where userID = @userID
-
-   if @balance < @offerAmount
-		Raiserror('Error: Insufficient Balance!',11,1)
-   
-   else
 	 update offer set offerAmount= @offerAmount where offerID = @offerID
 end
 
@@ -299,18 +286,13 @@ alter proc addOrder
 @orderingID int, @sellerID int, @stockID int,@orderAmount money, @offerID int
 as
 begin
-
-   declare @balance money 
 	DECLARE @errValue int	
-	select @balance = balance from users where userID = @orderingID
-
-   if @balance < @orderAmount
-		Raiserror('Error: Buyer has insufficient Balance!',11,1)
-    
-   else
-	begin
-	
-	begin transaction
+    begin transaction
+		insert into orders(orderingUserID, sellerUserID, stockID,orderAmount) values(@orderingID, @sellerID, @stockID,@orderAmount)
+		SET @errValue = @@ERROR
+		IF @errValue > 0
+			ROLLBACK TRANSACTION
+		
 		update offer set offerstatus = 'Accepted' where offerID = @offerID
 		SET @errValue = @@ERROR
 		IF @errValue > 0
@@ -330,28 +312,33 @@ begin
 		SET @errValue = @@ERROR
 		IF @errValue > 0
 			ROLLBACK TRANSACTION
-		insert into orders(orderingUserID, sellerUserID, stockID,orderAmount) values(@orderingID, @sellerID, @stockID,@orderAmount)
-		SET @errValue = @@ERROR
-		IF @errValue > 0
-			ROLLBACK TRANSACTION
+		
 	commit transaction
 	
-	end
+	--end
 end
-
 
 alter proc viewMyOrder
 @userID int
 as
 	select * from orderList where orderingUserID = @userID
+	
+create proc searchMyOrder
+@userID int, @search varchar(30)
+as
+	select * from orderList where orderingUserID = @userID and (concat(companyName,companytype)) like '%'+@search+'%'
 
 create proc viewRecievedOrder
 @userID int
 as
 	select * from orderList where sellerUserID = @userID
 	
-	select *from users
-
+create proc searchrecievedOrder
+@userID int, @search varchar(30)
+as
+	select * from orderList where sellerUserID = @userID and (concat(companyName,companytype)) like '%'+@search+'%'
+	
+	
 alter proc cancelOrder
 @orderID int, @sellerID int, @buyerID int
 as
@@ -380,15 +367,17 @@ begin
 end
 
 
-Create procedure viewOrdersForAdmin
+alter procedure viewOrdersForAdmin
 as
 	select *from orderData
 
+
+	/*
 create procedure searchOrdersForAdmin
 @search varchar(30)
 as
 	select *from orderData where (concat(orderer,orderee, companyName, companyType)) like '%'+@search+'%' 
-
+	*/
 
 -------------------------------------------------------------------------------------------------------------------------------------------
 --Order procedures end
@@ -414,8 +403,6 @@ begin
 	return @age
 end
 
-declare @date date = gETDATE()
-print @date
 create function fetchID(@username varchar(30))
 returns int
 as
@@ -423,6 +410,105 @@ begin
 	return (select userid from users where username = @username)
 end
 
+create function searchListing(@search varchar(30))
+returns table
+as
+return
+(
+	select *from viewListings where (concat(username,companyName, companyType, status)) like '%'+@search+'%'
+)
+
+alter function searchOffersForAdmin(@search varchar(30))
+returns table
+as
+return
+(
+	select *from offerData where (concat(offerer,offeree, companyName, companyType, offerStatus)) like '%'+@search+'%' 
+)
+
+alter function searchOrdersForAdmin(@search varchar(30))
+returns table
+as
+return
+(
+	select *from orderData where (concat(orderer,orderee, companyName, companyType)) like '%'+@search+'%' 
+)
+
+create function searchUser(@search varchar(30))
+returns table
+as
+return
+(
+	select *from users where (concat(firstName, lastName, phoneNumber, username)) like '%'+@search+'%'	
+)
+
+
+
+------------------------------------------------------------------------------------------------------------------------------
+--Triggers
+------------------------------------------------------------------------------------------------------------------------------
+
+create trigger trgDenyAccessToAdminTable
+on admin
+for insert, update, delete
+as
+begin
+	declare @currentUser varchar(30) = SUSER_NAME()
+	if @currentUser =  'userlogin'
+	begin
+		Print 'User cant access admin table'
+		ROLLBACK TRANSACTION
+	end
+end
+
+create trigger trgCheckBalanceForOffer
+on offer
+for insert, update
+as
+begin
+	declare @balance money, @offerAmount money, @offeringID int
+	set @offeringID = (select offeringUserID from inserted)
+	set @offerAmount = (select offerAmount from inserted)
+	select @balance = balance from users where userID = @offeringID
+
+	if @balance < @offerAmount
+	begin
+		Raiserror('Error: Insufficient Balance!',11,1)
+		rollback transaction
+	end
+end
+
+alter trigger trgCheckGender
+on users
+for insert, update
+as
+begin
+	declare @gender varchar(10) = (select gender from inserted)
+	
+	if @gender != 'male' and @gender != 'female'
+	begin
+		raiserror('Error! Invalid Gender',11,1)
+		rollback transaction
+	end
+end
+
+
+alter trigger trgCheckBalanceForOrder
+on orders
+for insert, update
+as
+begin
+	declare @balance money, @orderAmount money, @orderingID int
+	set @orderingID = (select orderingUserID from inserted)
+	set @orderAmount = (select orderAmount from inserted)
+	select @balance = balance from users where userID = @orderingID
+	
+	if @balance < @orderAmount
+	begin
+		Raiserror('Error: Buyer has insufficient Balance!',11,1)
+		rollback transaction
+	end
+end
 
 
 
